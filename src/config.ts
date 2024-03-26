ef123b3ab24f1c8fe789cfb980c3e8b9933c9e98
Marjan Kalanaki <marjan.kalanaki@guardian.co.uk>
2024-03-26 10:25:03 +0000

    use correct stage value

--- src/config.ts
+++ src/config.ts
@@ -19,7 +19,7 @@ const credentialProvider = (onAws: boolean) =>
 
 export const getConfig = async (): Promise<ParliamentQuestionConfig> => {
 	const region = 'eu-west-1';
-	const stage = 'DEV'; //await getEnvVarOrMetadata('STAGE', 'tags/instance/Stage');
+	const stage = process.env['STAGE'];
 	const ssm = new SSM({
 		region,
 		credentials: credentialProvider(stage !== 'DEV'),
